655b4d8d76ed59e55d0cd1f287990f83a377757a
Xxamanie <sirjvincent@gmail.com>
2026-01-26 19:07:17 +0100

    !!!!!!!!!!!!

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import ErrorBoundary from './components/ErrorBoundary';
-import './index.css';
+import App from '../App';
+import ErrorBoundary from '../components/ErrorBoundary';
+import '../index.css';
 
 const container = document.getElementById('root');
 if (!container) {

1468728631857e42dd31f3d92993d6764df8e6ee
Xxamanie <sirjvincent@gmail.com>
2026-05-02 18:57:25 +0100

    hmm

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { ContactSection, PageHero } from '../components/SiteSections';
+import { ContactSection } from '../components/SiteSections';
 
 const ContactPage: React.FC = () => {
   return (
     <>
-      <PageHero
-        eyebrow="Contact"
-        title={<>Start a conversation with <em>Orange Hat</em></>}
-        lead="If you want to give, volunteer, ask a question, or explore a partnership, this page is the direct route."
-        image="/images/joel.jpg"
-      />
       <ContactSection />
     </>
   );
